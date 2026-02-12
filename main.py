f8115bdb33fe2deaacd1b32c4b459456c35e84e3
SerKin0 <71343548+SerKin0@users.noreply.github.com>
2026-02-13 00:08:19 +0300

    Редактирование gitignore и удаление pycache

--- main.py
+++ main.py
@@ -1,37 +1,20 @@
+import asyncio
+
 from aiogram import Bot, Dispatcher
-from aiogram.filters import Command
-from aiogram.types import Message
 
+from source.handlers import buttons, commands
 from source.settings import TOKEN_BOT
-from source.logs.logger import logger
-from source.keyboards.reply import create_start_keyboard
 
 
-bot = Bot(token=TOKEN_BOT)
-dp = Dispatcher()
+async def main():
+    bot = Bot(token=TOKEN_BOT)
+    dp = Dispatcher()
+
+    dp.include_routers(commands.router_commands, buttons.router_buttons)
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
 
 
-@dp.message(Command("start"))
-async def start_command(message: Message):
-    await message.answer("""✨ Добро пожаловать в бота «Что подарить?»! ✨
-
-Тот самый помощник, который навсегда решит проблему с подарками.
-
-Создайте свой вишлист, добавьте в него ссылки на товары из любых магазинов и легко делитесь списком с друзьями и семьей.
-
-Давайте создадим ваш первый список желаний! Просто напишите, что бы вы хотели получить в подарок?""",
-    reply_markup=create_start_keyboard())
-    logger.info(f"Start message")
-
-
-@dp.message(Command("help"))
-async def help_command(message: Message):
-    await  message.answer("""""")
-
-
-@dp.message(Command(commands="data"))
-async def get_data_user(message: Message):
-    print(message)
-
-if __name__ == '__main__':
-    dp.run_polling(bot)
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ source/handlers/commands.py
@@ -0,0 +1,28 @@
+from aiogram import Router
+from aiogram.filters import Command
+from aiogram.types import Message
+
+from source.keyboards.reply import create_start_keyboard
+from source.logs.logger import logger
+
+router_commands = Router()
+
+
+@router_commands.message(Command("start"))
+async def start_command(message: Message):
+    await message.answer(
+        """✨ Добро пожаловать в бота «Что подарить?»! ✨
+
+Тот самый помощник, который навсегда решит проблему с подарками.
+
+Создайте свой вишлист, добавьте в него ссылки на товары из любых магазинов и легко делитесь списком с друзьями и семьей.
+
+Давайте создадим ваш первый список желаний! Просто напишите, что бы вы хотели получить в подарок?""",
+        reply_markup=create_start_keyboard(),
+    )
+    logger.info("Стартовое сообщение")
+
+
+@router_commands.message(Command("help"))
+async def help_command(message: Message):
+    await message.answer("""""")
