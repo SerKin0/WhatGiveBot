8f43ef64e94a903b0fccf3e172509121b7eb27f1
SerKin0 <71343548+SerKin0@users.noreply.github.com>
2026-02-13 20:52:42 +0300

    Удаление файлов

--- source/logs/logger.py
+++
@@ -1,7 +0,0 @@
-import logging
-
-logging.basicConfig(
-    level=logging.DEBUG, format="[%(asctime)s] %(levelname)-8s %(filename)s:%(lineno)d - %(name)s - %(message)s"
-)
-
-logger = logging.getLogger(__name__)
